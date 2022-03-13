eb7b88918c8992557bfbdcb1f94c9b62587597b1
Mario Scalas <mario.scalas@gmail.com>
2022-03-13 17:17:56 +0000

    Added lambda handler for note deletion.

--- lambda/mynotes/port/note.py
+++ lambda/mynotes/port/note.py
@@ -7,6 +7,7 @@ from mynotes.adapter.s3_bucket_adapter import S3BucketAdapter
 from mynotes.core.architecture import User
 from mynotes.core.notes import NoteUseCases
 from mynotes.port import lambda_utils
+from mynotes.port.exception_management import with_exception_management
 
 object_store = S3BucketAdapter(
     boto3.resource("s3"),
@@ -17,9 +18,9 @@ note_repository = DynamoDBNoteRepository()
 
 usecase = NoteUseCases(object_store, note_repository)
 
+@with_exception_management
 def handler_create_note(event, context) -> dict:
-    """Main entrypoint for this AWS Lambda function: it routes the AWS Event (Application Gateway) to
-    the correct business logic handler.
+    """Handler for creating a new note.
     Args:
         event: the AWS Lambda event
         context: the AWS Lambda execution context
@@ -32,20 +33,22 @@ def handler_create_note(event, context) -> dict:
     json_body = lambda_utils.get_json_body(event)
     id = json_body.get("id", None)
     content = json_body.get("content")    
+    tags = json_body.get("tags", None)
 
     # TODO Get user from authentication
     username = "mario"
 
     note = usecase.create_note(
-        User("mario"),
-        content
+        User(username),
+        content,
+        tags
     )
 
     return lambda_utils.to_json_response(note)
 
+@with_exception_management
 def handler_get_by_id(event, context) -> dict:
-    """Main entrypoint for this AWS Lambda function: it routes the AWS Event (Application Gateway) to
-    the correct business logic handler.
+    """Handler for returning a note by its id.
     Args:
         event: the AWS Lambda event
         context: the AWS Lambda execution context
@@ -60,3 +63,24 @@ def handler_get_by_id(event, context) -> dict:
     note = usecase.find_note_by_id(id)
 
     return lambda_utils.to_json_response(note)
+
+@with_exception_management
+def handler_delete_by_id(event, context) -> dict:
+    """Handler for deleting a note.
+    Args:
+        event: the AWS Lambda event
+        context: the AWS Lambda execution context
+    
+    Returns:
+        a dict suitable as AWS Lambda response
+    """
+    print(json.dumps(event))
+
+    id = lambda_utils.get_path_parameter("id")
+
+    usecase.delete_note_by_id(id)
+
+    return {
+        "status": 204
+    }
+
